3b2592a4e01f5634f234b85041151d0528282d2e
Yuragranovsky <yuragranovsky@gmail.com>
2023-07-09 21:13:39 +0300

    CW #28 PrintCalendar

--- src/telran/time/test/DateTimeTests.java
+++ src/telran/time/test/DateTimeTests.java
@@ -1,4 +1,4 @@
-package telran.time;
+package telran.time.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -7,6 +7,7 @@ import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.TemporalAdjuster;
 import java.time.temporal.UnsupportedTemporalTypeException;
+import telran.time.*;
 class DateTimeTests {
 
 	@Test
@@ -27,23 +28,22 @@ class DateTimeTests {
 		TemporalAdjuster fr13 = new NextFriday13();
 		ZonedDateTime zdt = ZonedDateTime.now();
 		ZonedDateTime fr13Expected = ZonedDateTime.of(2023, 10, 13, 0, 0, 0, 0, ZoneId.systemDefault());
-		assertEquals(fr13Expected, zdt.with(fr13));
+		assertEquals(fr13Expected.toLocalDate(), zdt.with(fr13).toLocalDate());
 		LocalDate fr13Expected2 = LocalDate.of(2024, 9, 13);
 		LocalDate ld = LocalDate.of(2023, 10, 13);
 		assertEquals(fr13Expected2, ld.with(fr13));
+		assertThrowsExactly(UnsupportedTemporalTypeException.class,
+				()->LocalTime.now().with(fr13));
 		
 	}
 	@Test
 	void canadaCurrentTime() {
 		//displayCurrentTime("Europe/London");
-		//TODO display current date & time in all time zones related to Canada
+		// display current date & time in all time zones related to Canada
 		//Date / Time (HH:mm) / Time Zone name
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/YYYY / HH:mm / zzzz");
-		for(String zoneName: ZoneId.getAvailableZoneIds()) {
-			if (zoneName.contains("Canada")) {
-				displayCurrentTime(zoneName, dtf) ;
-			}
-		}
+		ZoneId.getAvailableZoneIds().stream().filter(z -> z.contains("Canada"))
+		.forEach(z -> displayCurrentTime(z, dtf));
 		
 	}
 	void displayCurrentTime(String zoneName, DateTimeFormatter dtf) {
